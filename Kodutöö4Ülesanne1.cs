fed84899108461c4fc231afcacc0d94bf7da3018
ReneVainula <163726426+ReneVainula@users.noreply.github.com>
2024-12-08 19:47:38 +0200

    Create Kodutöö4Ülesanne1.cs

---
+++ Kodutöö4Ülesanne1.cs
@@ -0,0 +1,13 @@
+// Task 1: Hashing
+
+/* 1. Räsifunktsioon on mis tahes funktsioon, mis kaardistab suvalised andmemahud fikseeritud suurusega väärtusteks.
+Räsi kasutatakse salvestatud andmete indekseerimisel, et vähendada plahvatuslikult suuremate objektide otsinguaega. 
+Neid kasutatakse ka vahemälu loomiseks, mis lühendab ka pikkade toimingute täitmise aega, tagastades lihtsalt teadaoleva sisestusandmete komplekti lõpptulemuse.
+
+2. Hea räsifunktsioon on käivitub kiiresti ja minimeerib dubleeritud tulemuste väärtusi, välja arvatud juhul, kui ülesanne nõuab konkreetselt kokkupõrkeid.
+
+3. Kokkupõrkelahendust saab käsitleda erineval viisil, olenevalt kasutatava räsitabeli tüübist. 
+Avatud adresseerimiseks, kui kokkupõrge leitakse, hakkab funktsioon proovima piki massiivi, kuni leiab vaba pesa. 
+Eraldi aheldamiseks on iga massiivi pesa lingitud loendi algus ja kõik põrkuvad väärtused lisatakse loendi taha. 
+Teine kokkupõrke lahendamise tehnika on lihtsalt vana väärtuse ülekirjutamine uusimaga.
+*/
