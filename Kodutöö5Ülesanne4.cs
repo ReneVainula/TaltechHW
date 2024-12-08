6c5ac4ff10b4a092324df41c397faf67a3976d33
ReneVainula <163726426+ReneVainula@users.noreply.github.com>
2024-12-08 20:05:31 +0200

    Create Kodutöö5Ülesanne4.cs

---
+++ Kodutöö5Ülesanne4.cs
@@ -0,0 +1,12 @@
+// Task 4: Overview of the Red-Black Tree
+
+/* 1. Punane-must puu on binaarse otsingupuu eritüüp, mida kasutatakse võrreldavate andmete korraldamiseks. Sellel on järgmised omadused:
+Iga sõlm on kas punane või must
+Kõik tühjad sõlmed loetakse mustaks
+Punasel sõlmel pole punast last
+Iga tee antud sõlmest mõne selle järeltulija tühja sõlmeni läbib sama arvu musti sõlme.
+
+2. Kui sõlmel N on täpselt üks laps, peab laps olema punane, sest kui see oleks must, istuksid selle tühjad järeltulijad teistsugusel musta sügavusel kui N-i tühjal lapsel, rikkudes viimast reeglit.
+
+3. Punased-mustad puud pakuvad halvimaid sisestus-, kustutamis- ja ja otsinguaja garantiisid. Sellisena sobivad need ideaalselt reaalajas rakenduste jaoks ja on aluseks paljudele teistele andmestruktuuridele.
+*/
