7871a51812468057997fe021073335353df8e716
ReneVainula <163726426+ReneVainula@users.noreply.github.com>
2024-12-08 20:01:28 +0200

    Create Kodutöö5Ülesanne3.cs

---
+++ Kodutöö5Ülesanne3.cs
@@ -0,0 +1,6 @@
+// Task 3: Theoretical Analysis of a Binary Search Tree (BST)
+
+/* 1. Binaarne otsingupuu on sorteeritud puustruktuur, kus vasakpoolne alamsõlm on alati väiksem kui emasõlm ja parem alamsõlm on alati suurem kui emasõlm.
+
+2. Binaarse otsingu mõte on eemaldada igal sammul pool võimalikest väärtustest. Tasakaalustamata puu rikuks selle eelduse ja nõuab seega rohkem toiminguid. Ajaline keerukus oleks vahemikus O(logN) kuni O(N)
+*/
