68f79095fb46517a27b84f662041319721aadd99
ReneVainula <163726426+ReneVainula@users.noreply.github.com>
2024-12-08 19:23:12 +0200

    Create Kodutöö2Ülesanne4.cs

---
+++ Kodutöö2Ülesanne4.cs
@@ -0,0 +1,6 @@
+// Task 4: Time Complexity Analysis (1 point)
+/* a)
+Kõigil neil on halvimal juhul O(n²) keerukus
+ b)
+Sisestamise sortimine oleks kiireim, kuna see on optimeeritud väiksemate loendite jaoks ja sellel on vähem vahetusi kui mulli sortimisel, kuigi suhteliselt parimal juhul võiks mulli sortimine lõppeda esimese iteratsiooniga.
+*/
